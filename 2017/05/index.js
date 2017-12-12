674126ebcb460d938f254ee4eda35cdca874ff8b
Joshua Morris <me@joshuamorris.info>
2017-12-11 23:48:59 -0800

    Day five complete

--- 2017/05/index.js
+++ 2017/05/index.js
@@ -1,37 +1,39 @@
+'use strict';
+
 const fs = require('fs');
 
 const soln1 = (input) => {
-    let index = 0, jumps = 0;
-    input = input.map(i => +i);
+  let index = 0, jumps = 0;
+  input = input.map(i => +i);
 
-    do {
-        let distance = input[index];
-        input[index]++;
-        index += distance;
-        jumps++;
-    } while (index < input.length && index >= 0);
-    return jumps;
+  do {
+    let distance = input[index];
+    input[index]++;
+    index += distance;
+    jumps++;
+  } while (index < input.length && index >= 0);
+  return jumps;
 };
 
 const soln2 = (input) => {
-    let index = 0, jumps = 0;
-    input = input.map(i => +i);
+  let index = 0, jumps = 0;
+  input = input.map(i => +i);
 
-    do {
-        let distance = input[index];
-        if (distance > 2) input[index]--;
-        else input[index]++;
-        index += distance;
-        jumps++
-    } while (index < input.length && index >= 0);
-    return jumps;
+  do {
+    let distance = input[index];
+    if (distance > 2) input[index]--;
+    else input[index]++;
+    index += distance;
+    jumps++;
+  } while (index < input.length && index >= 0);
+  return jumps;
 };
 
-let test = fs.readFileSync(`./test.txt`, 'utf-8').trimRight().split('\n');
-let input = fs.readFileSync(`./input.txt`, 'utf-8').trimRight().split('\n');
+let test = fs.readFileSync('./test.txt', 'utf-8').trimRight().split('\n');
+let input = fs.readFileSync('./input.txt', 'utf-8').trimRight().split('\n');
 
-console.assert(5 === soln1(test));
-console.assert(10 === soln2(test));
+console.assert(soln1(test) === 5);
+console.assert(soln2(test) === 10);
 
 console.log('Part 1: ', soln1(input));
 console.log('Part 2: ', soln2(input));
